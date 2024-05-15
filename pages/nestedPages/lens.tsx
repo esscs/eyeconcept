07ae1071dad77cabfa0db4c14de8cd0fcf165a0a
Mohammed-Shoaib01 <65510889+Mohammed-Shoaib01@users.noreply.github.com>
2024-05-16 00:21:49 +0300

    updated the json
    Updated the json for the last 3 lens types for them to work, also made some naming changes regarding "TL 1.x" based on clients requests.

--- pages/nestedPages/lens.tsx
+++ pages/nestedPages/lens.tsx
@@ -12,9 +12,9 @@ const lensData = {
     },
     hi_index_material: {
       "polycarbonate_1.59": 50,
-      tl16_max_4: 80,
-      tl167_max_7: 190,
-      tl174_max_4: 210,
+      "TL 1.6": 80,
+      "TL 1.67": 190,
+      "TL 1.74": 210,
     },
     coatings: {
       hard_coat_hc: 49,
@@ -51,9 +51,9 @@ const lensData = {
     },
     hi_index_material: {
       "polycarbonate_1.59": 50,
-      tl16_max_4: 80,
-      tl167_max_7: 190,
-      tl174_max_4: 210,
+      "TL 1.6": 80,
+      "TL 1.67": 190,
+      "TL 1.74": 210,
     },
     coatings: {
       hard_coat_hc: 49,
@@ -92,8 +92,8 @@ const lensData = {
     },
     hi_index_material: {
       "polycarbonate_1.59": 50,
-      tl16_max_4: 80,
-      tl167_max_7: 190,
+      "TL 1.6": 80,
+      "TL 1.67": 190,
     },
     coatings: {
       crizal_rock: 249,
@@ -126,9 +126,9 @@ const lensData = {
     },
     hi_index_material: {
       "polycarbonate_1.59": 50,
-      tl16_max_4: 80,
-      tl167_max_7: 190,
-      tl174_max_4: 210,
+      "TL 1.6": 80,
+      "TL 1.67": 190,
+      "TL 1.74": 210,
     },
     coatings: {
       hard_coat_hc: 49,
@@ -153,12 +153,14 @@ const lensData = {
     },
   },
   "essilor_line_bi-focal_lens": {
-    ft_28: 219,
-    ft_35: 219,
-    round_seg: 219,
+    options: {
+      "FT 28": 219,
+      "FT 35": 219,
+      "Round Seg": 219,
+    },
     hi_index_material: {
       "polycarbonate_1.59": 50,
-      tl16_max_4: 80,
+      "TL 1.6": 80,
     },
     coatings: {
       hard_coat_hc: 49,
@@ -232,10 +234,10 @@ const lensData = {
       ar_coating: 150,
     },
     add_ons: {
-      transitions_gen_s: {
-        gray_black_brown: 200,
-        special_colors: 300,
-      },
+    
+      transitions_gen_s_gray_black_brown: 200,
+      transitions_gen_s_special_colors: 300,
+      
 
       "custom_engraving 2_letters": 100,
       "custom_engraving 4_letters": 150,
