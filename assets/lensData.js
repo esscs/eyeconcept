9093f6151d6c55c99df1b39a4e46df06a2410c55
b1gh3ro <saadmujeeb1008@gmail.com>
2024-06-02 06:08:22 +0530

    made lensData.js better

--- assets/lensData.js
+++ assets/lensData.js
@@ -1,244 +1,240 @@
 export default lensData = {
-    essilor_single_vision_lens: {
-      options: {
-        single_vision_standard: 148,
-        eyezan: 248,
-        stellest_w_crizal_rock: 650,
-      },
-      hi_index_material: {
-        "polycarbonate_1.59": 50,
-        tl16_max_4: 80,
-        tl167_max_7: 190,
-        tl174_max_4: 210,
-      },
-      coatings: {
-        hard_coat_hc: 49,
-        hmc_ec_anti_glare_anti_scratch: 98,
-        hmc_ec_blue_capture: 148,
-        crizal_easy_pro: 198,
-        crizal_rock: 249,
-        crizal_prevencia: 298,
-        crizal_mirror_coating_hmc_sun: 298,
-      },
-      add_ons: {
-        solid_tint: 70,
-        gradient_tint: 90,
-        solid_polarized: 149,
-        gradient_polarized: 209,
-        any_custom_tints_colors: 130,
-        transitions_gen_s: 149,
-        "transitions_xtractive_2.0": 198,
-      },
-      extras: {
-        high_cyl: 40,
-        prism_charge: 60,
-        any_custom_shape_sizing: 50,
-        patient_own_frame_lab_charge_edging_mounting: 50,
-        patient_own_frame_lab_charge_drilling_mounting: 80,
-        custom_polarized_clip_on: 169,
-      },
+  "Essilor Single Vision Lens": {
+    "Options": {
+      "Single Vision Standard": 148,
+      "Eyezan": 248,
+      "Stellest w/ Crizal Rock": 650
     },
-    essilor_progressive_lens: {
-      options: {
-        ds_one: 298,
-        distinctive_expand_iv: 398,
-        distinctive_superior: 498,
-      },
-      hi_index_material: {
-        "polycarbonate_1.59": 50,
-        tl16_max_4: 80,
-        tl167_max_7: 190,
-        tl174_max_4: 210,
-      },
-      coatings: {
-        hard_coat_hc: 49,
-        hmc_ec_anti_glare_anti_scratch: 98,
-        hmc_ec_blue_capture: 148,
-        crizal_easy_pro: 198,
-        crizal_rock: 249,
-        crizal_prevencia: 298,
-        crizal_mirror_coating_hmc_sun: 298,
-      },
-      add_ons: {
-        solid_tint: 70,
-        gradient_tint: 90,
-  
-        solid_polarized: 149,
-        gradient_polarized: 209,
-  
-        any_custom_tints_colors: 130,
-        transitions_gen_s: 149,
-        "transitions_xtractive_2.0": 198,
-      },
-      extras: {
-        high_cyl: 40,
-        prism_charge: 60,
-        any_custom_shape_sizing: 50,
-        patient_own_frame_lab_charge_edging_mounting: 50,
-        patient_own_frame_lab_charge_drilling_mounting: 80,
-        custom_polarized_clip_on: 169,
-      },
+    "Hi Index Material": {
+      "Polycarbonate 1.59": 50,
+      "TL 1.6": 80,
+      "TL 1.67": 190,
+      "TL 1.74": 210
     },
-    essilor_varilux_progressive_lens: {
-      options: {
-        "varilux_physio_3.0": 598,
-        varilux_e_series: 698,
-        varilux_x_series: 898,
-      },
-      hi_index_material: {
-        "polycarbonate_1.59": 50,
-        tl16_max_4: 80,
-        tl167_max_7: 190,
-      },
-      coatings: {
-        crizal_rock: 249,
-        crizal_prevencia: 298,
-      },
-      add_ons: {
-        solid_tint: 70,
-        gradient_tint: 90,
-  
-        solid_polarized: 149,
-        gradient_polarized: 209,
-  
-        any_custom_tints_colors: 130,
-        transitions_gen_s: 149,
-        "transitions_xtractive_2.0": 198,
-      },
-      extras: {
-        high_cyl: 40,
-        prism_charge: 60,
-        any_custom_shape_sizing: 50,
-        patient_own_frame_lab_charge_edging_mounting: 50,
-        patient_own_frame_lab_charge_drilling_mounting: 80,
-        custom_polarized_clip_on: 169,
-      },
+    "Coatings": {
+      "Hard Coat (HC)": 49,
+      "HMC/EC (Anti-Glare, Anti-Scratch)": 98,
+      "HMC/EC (Blue Capture)": 148,
+      "Crizal Easy Pro": 198,
+      "Crizal Rock": 249,
+      "Crizal Prevencia": 298,
+      "Crizal Mirror Coating (HMC Sun)": 298
     },
-    essilor_computer_progressive_lens: {
-      options: {
-        distinctive_computer: 298,
-        varilux_digitime: 448,
-      },
-      hi_index_material: {
-        "polycarbonate_1.59": 50,
-        tl16_max_4: 80,
-        tl167_max_7: 190,
-        tl174_max_4: 210,
-      },
-      coatings: {
-        hard_coat_hc: 49,
-        hmc_ec_anti_glare_anti_scratch: 98,
-        hmc_ec_blue_capture: 148,
-        crizal_easy_pro: 198,
-        crizal_rock: 249,
-        crizal_prevencia: 298,
-        crizal_mirror_coating_hmc_sun: 298,
-      },
-      add_ons: {
-        transitions_gen_s: 149,
-        "transitions_xtractive_2.0": 198,
-      },
-      extras: {
-        high_cyl: 40,
-        prism_charge: 60,
-        any_custom_shape_sizing: 50,
-        patient_own_frame_lab_charge_edging_mounting: 50,
-        patient_own_frame_lab_charge_drilling_mounting: 80,
-        custom_polarized_clip_on: 169,
-      },
+    "Add-Ons": {
+      "Solid Tint": 70,
+      "Gradient Tint": 90,
+      "Solid Polarized": 149,
+      "Gradient Polarized": 209,
+      "Any Custom Tints/Colors": 130,
+      "Transitions Gen 8": 149,
+      "Transitions XTRActive 2.0": 198
     },
-    "essilor_line_bi-focal_lens": {
-      options:{ft_28: 219,
-      ft_35: 219,
-      round_seg: 219,},
-      hi_index_material: {
-        "polycarbonate_1.59": 50,
-        tl16_max_4: 80,
-      },
-      coatings: {
-        hard_coat_hc: 49,
-        hmc_ec_anti_glare_anti_scratch: 98,
-        hmc_ec_blue_capture: 148,
-        crizal_easy_pro: 198,
-        crizal_rock: 249,
-        crizal_prevencia: 298,
-        crizal_mirror_coating_hmc_sun: 298,
-      },
-      add_ons: {
-        solid_tint: 70,
-        gradient_tint: 90,
-        solid_polarized: 149,
-        gradient_polarized: 209,
-  
-        any_custom_tints_colors: 130,
-        transitions_gen_s: 149,
-      },
-      extras: {
-        high_cyl: 40,
-        prism_charge: 60,
-        any_custom_shape_sizing: 50,
-        patient_own_frame_lab_charge_edging_mounting: 50,
-        patient_own_frame_lab_charge_drilling_mounting: 80,
-        custom_polarized_clip_on: 169,
-      },
+    "Extras": {
+      "High Cyl": 40,
+      "Prism Charge": 60,
+      "Any Custom Shape/Sizing": 50,
+      "Patient Own Frame Lab Charge (Edging/Mounting)": 50,
+      "Patient Own Frame Lab Charge (Drilling/Mounting)": 80,
+      "Custom Polarized Clip-On": 169
+    }
+  },
+  "Essilor Progressive Lens": {
+    "Options": {
+      "DS One": 298,
+      "Distinctive Expand IV": 398,
+      "Distinctive Superior": 498
     },
-    custom_cartier_single_vision_lens: {
-      options: {
-        custom_single_vision_non_diamond_cut_less_2: 550,
-        custom_single_vision_diamond_cut_less_2: 750,
-      },
-      hi_index_material: {
-        hi_index_thinner_lenses_2_to_4: 150,
-        hi_index_thinner_lenses_4_to_6: 200,
-        hi_index_thinner_lenses_greater_6: 250,
-      },
-      coatings: {
-        ar_coating: 150,
-      },
-      add_ons: {
-        gray_black_brown: 200,
-        special_colors: 300,
-        "custom_engraving 2_letters": 100,
-        "custom_engraving 4_letters": 150,
-        "custom_engraving more_than_4_letters": 200,
-        colour_in_engraving_gold_silver: 50,
-      },
-      extras: {
-        high_cyl: 80,
-        prism_charge: 100,
-        patient_own_frame_lab_charge_edging_mounting: 50,
-        patient_own_frame_lab_charge_drilling_mounting: 100,
-      },
+    "Hi Index Material": {
+      "Polycarbonate 1.59": 50,
+      "TL 1.6": 80,
+      "TL 1.67": 190,
+      "TL 1.74": 210
     },
-    custom_cartier_progressive_lens: {
-      options: {
-        custom_progressive_standard_non_diamond_cut_less_2: 1100,
-        "custom_progressive_varilux_physio_3.0_non_diamond_cut_less_2": 1400,
-        custom_progressive_standard_diamond_cut_less_2: 1200,
-        "custom_progressive_varilux_physio_3.0_diamond_cut_less_2": 1500,
-      },
-      hi_index_material: {
-        hi_index_thinner_lenses_2_to_4: 150,
-        hi_index_thinner_lenses_4_to_6: 200,
-        hi_index_thinner_lenses_greater_6: 250,
-      },
-      coatings: {
-        ar_coating: 150,
-      },
-      add_ons: {
-        "transitions_gen_s gray_black_brown": 200,
-        "transitions_gen_s special_colors": 300,
-        "custom_engraving 2_letters": 100,
-        "custom_engraving 4_letters": 150,
-        "custom_engraving more_than_4_letters": 200,
-        colour_in_engraving_gold_silver: 50,
-      },
-      extras: {
-        high_cyl: 80,
-        prism_charge: 100,
-        patient_own_frame_lab_charge_edging_mounting: 50,
-        patient_own_frame_lab_charge_drilling_mounting: 100,
-      },
+    "Coatings": {
+      "Hard Coat (HC)": 49,
+      "HMC/EC (Anti-Glare, Anti-Scratch)": 98,
+      "HMC/EC (Blue Capture)": 148,
+      "Crizal Easy Pro": 198,
+      "Crizal Rock": 249,
+      "Crizal Prevencia": 298,
+      "Crizal Mirror Coating (HMC Sun)": 298
     },
-  };
-  
+    "Add-Ons": {
+      "Solid Tint": 70,
+      "Gradient Tint": 90,
+      "Solid Polarized": 149,
+      "Gradient Polarized": 209,
+      "Any Custom Tints/Colors": 130,
+      "Transitions Gen 8": 149,
+      "Transitions XTRActive 2.0": 198
+    },
+    "Extras": {
+      "High Cyl": 40,
+      "Prism Charge": 60,
+      "Any Custom Shape/Sizing": 50,
+      "Patient Own Frame Lab Charge (Edging/Mounting)": 50,
+      "Patient Own Frame Lab Charge (Drilling/Mounting)": 80,
+      "Custom Polarized Clip-On": 169
+    }
+  },
+  "Essilor Varilux Progressive Lens": {
+    "Options": {
+      "Varilux Physio 3.0": 598,
+      "Varilux E Series": 698,
+      "Varilux X Series": 898
+    },
+    "Hi Index Material": {
+      "Polycarbonate 1.59": 50,
+      "TL 1.6": 80,
+      "TL 1.67": 190
+    },
+    "Coatings": {
+      "Crizal Rock": 249,
+      "Crizal Prevencia": 298
+    },
+    "Add-Ons": {
+      "Solid Tint": 70,
+      "Gradient Tint": 90,
+      "Solid Polarized": 149,
+      "Gradient Polarized": 209,
+      "Any Custom Tints/Colors": 130,
+      "Transitions Gen 8": 149,
+      "Transitions XTRActive 2.0": 198
+    },
+    "Extras": {
+      "High Cyl": 40,
+      "Prism Charge": 60,
+      "Any Custom Shape/Sizing": 50,
+      "Patient Own Frame Lab Charge (Edging/Mounting)": 50,
+      "Patient Own Frame Lab Charge (Drilling/Mounting)": 80,
+      "Custom Polarized Clip-On": 169
+    }
+  },
+  "Essilor Computer Progressive Lens": {
+    "Options": {
+      "Distinctive Computer": 298,
+      "Varilux Digitime": 448
+    },
+    "Hi Index Material": {
+      "Polycarbonate 1.59": 50,
+      "TL 1.6": 80,
+      "TL 1.67": 190,
+      "TL 1.74": 210
+    },
+    "Coatings": {
+      "Hard Coat (HC)": 49,
+      "HMC/EC (Anti-Glare, Anti-Scratch)": 98,
+      "HMC/EC (Blue Capture)": 148,
+      "Crizal Easy Pro": 198,
+      "Crizal Rock": 249,
+      "Crizal Prevencia": 298,
+      "Crizal Mirror Coating (HMC Sun)": 298
+    },
+    "Add-Ons": {
+      "Transitions Gen 8": 149,
+      "Transitions XTRActive 2.0": 198
+    },
+    "Extras": {
+      "High Cyl": 40,
+      "Prism Charge": 60,
+      "Any Custom Shape/Sizing": 50,
+      "Patient Own Frame Lab Charge (Edging/Mounting)": 50,
+      "Patient Own Frame Lab Charge (Drilling/Mounting)": 80,
+      "Custom Polarized Clip-On": 169
+    }
+  },
+  "Essilor Line Bi-Focal Lens": {
+    "Options": {
+      "FT 28": 219,
+      "FT 35": 219,
+      "Round Seg": 219
+    },
+    "Hi Index Material": {
+      "Polycarbonate 1.59": 50,
+      "TL 1.6": 80
+    },
+    "Coatings": {
+      "Hard Coat (HC)": 49,
+      "HMC/EC (Anti-Glare, Anti-Scratch)": 98,
+      "HMC/EC (Blue Capture)": 148,
+      "Crizal Easy Pro": 198,
+      "Crizal Rock": 249,
+      "Crizal Prevencia": 298,
+      "Crizal Mirror Coating (HMC Sun)": 298
+    },
+    "Add-Ons": {
+      "Solid Tint": 70,
+      "Gradient Tint": 90,
+      "Solid Polarized": 149,
+      "Gradient Polarized": 209,
+      "Any Custom Tints/Colors": 130,
+      "Transitions Gen 8": 149
+    },
+    "Extras": {
+      "High Cyl": 40,
+      "Prism Charge": 60,
+      "Any Custom Shape/Sizing": 50,
+      "Patient Own Frame Lab Charge (Edging/Mounting)": 50,
+      "Patient Own Frame Lab Charge (Drilling/Mounting)": 80,
+      "Custom Polarized Clip-On": 169
+    }
+  },
+  "Custom Cartier Single Vision Lens": {
+    "Options": {
+      "Custom Single Vision (Non-Diamond Cut, <2)": 550,
+      "Custom Single Vision (Diamond Cut, <2)": 750
+    },
+    "Hi Index Material": {
+      "Hi Index Thinner Lenses (2 to 4)": 150,
+      "Hi Index Thinner Lenses (4 to 6)": 200,
+      "Hi Index Thinner Lenses (>6)": 250
+    },
+    "Coatings": {
+      "AR Coating": 150
+    },
+    "Add-Ons": {
+      "Gray/Black/Brown": 200,
+      "Special Colors": 300,
+      "Custom Engraving (2 Letters)": 100,
+      "Custom Engraving (4 Letters)": 150,
+      "Custom Engraving (More Than 4 Letters)": 200,
+      "Colour in Engraving (Gold/Silver)": 50
+    },
+    "Extras": {
+      "High Cyl": 80,
+      "Prism Charge": 100,
+      "Patient Own Frame Lab Charge (Edging/Mounting)": 50,
+      "Patient Own Frame Lab Charge (Drilling/Mounting)": 100
+    }
+  },
+  "Custom Cartier Progressive Lens": {
+    "Options": {
+      "Custom Progressive Standard (Non-Diamond Cut, <2)": 1100,
+      "Custom Progressive Varilux Physio 3.0 (Non-Diamond Cut, <2)": 1400,
+      "Custom Progressive Standard (Diamond Cut, <2)": 1200,
+      "Custom Progressive Varilux Physio 3.0 (Diamond Cut, <2)": 1500
+    },
+    "Hi Index Material": {
+      "Hi Index Thinner Lenses (2 to 4)": 150,
+      "Hi Index Thinner Lenses (4 to 6)": 200,
+      "Hi Index Thinner Lenses (>6)": 250
+    },
+    "Coatings": {
+      "AR Coating": 150
+    },
+    "Add-Ons": {
+      "Transitions Gen 8 (Gray/Black/Brown)": 200,
+      "Transitions Gen 8 (Special Colors)": 300,
+      "Custom Engraving (2 Letters)": 100,
+      "Custom Engraving (4 Letters)": 150,
+      "Custom Engraving (More Than 4 Letters)": 200,
+      "Colour in Engraving (Gold/Silver)": 50
+    },
+    "Extras": {
+      "High Cyl": 80,
+      "Prism Charge": 100,
+      "Patient Own Frame Lab Charge (Edging/Mounting)": 50,
+      "Patient Own Frame Lab Charge (Drilling/Mounting)": 100
+    }
+  }
+}
